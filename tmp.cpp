4595ef2edc7fc989e915cc56ba2056d391cfe120
Hoaiduc195 <hoaiducduong19@gmail.com>
2025-06-20 15:32:21 +0700

    sorting

--- tmp.cpp
+++ tmp.cpp
@@ -1,53 +1,54 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+#include <iomanip>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int val;
     Node *left, *right;
-    Node(int x): val(x), left(nullptr), right(nullptr) {}
+    Node(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-Node* searchDel(Node* root, int k){
-    Node* cur = root;
-    Node* par = nullptr;
 
-    while(cur && cur->val!=k){
-        par = cur;
-        if(cur->val > k) cur = cur->left;
-        else cur = cur->right;
+void heapify(vector<int>& a, int size, int i){
+    int m = i;
+    int c1 = i*2+1, c2 = i*2+2;
+    if(c1<size && a[c1]>a[m]) m=c1;
+    if(c2<size && a[c2]>a[m]) m=c2;
+    if(m!=i){
+        swap(a[m], a[i]);
+        heapify(a, size, m);
     }
-    if(!cur) return root;
-
-
-    if(cur->left && cur->right){
-        Node* pre = cur->left;
-        Node* prePar = cur;
-
-        while(pre->right){
-            prePar = pre;
-            pre = pre->right;
-        }
-
-        cur->val = pre->val; 
-        par = prePar; cur = pre;
-    }
-
-    Node* child = (cur->left) ? cur->left: cur->right;
-
-    if(!par){
-        delete root; return child;
-    }
-
-    if(par->left == cur) par->left = child;
-    else par->right = child;
-
-    delete cur; 
-    return root;
 }
 
+void heapSort(vector<int>&a){
+    int n = a.size();
+    for(int i=0;i<n;i++) cout<<setw(3)<<i<<" ";
+    cout<<endl;
+    for(int i=(n-1)/2;i>=0;i--){
+        heapify(a, n, i);
+    }
+    for(int x:a) cout<<setw(3)<<x<<" ";
+    cout<<endl<<endl;
 
-int main() {
+    for(int i=n-1;i>=0;i--){
+        swap(a[i], a[0]);
+        for(int x:a) cout<<setw(3)<<x<<" ";
+        cout<<endl;
+        heapify(a, i, 0);
+        for(int x:a) cout<<setw(3)<<x<<" ";
+        cout<<endl;
+    }
+
+    for(int x:a) cout<<setw(3)<<x<<" ";
+}
+
+int main()
+{
+    vector<int> a = {5, 15, 12, 2, 10, 7, 4, 1, 9, 3};
+    heapSort(a);
 
     return 0;
 }
